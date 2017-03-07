c7b312a13d3f982f638442405ec58cc3a5d4011b
Benji Parish <benjaminparish628@gmail.com>
2017-03-06 22:54:31 -0600

    Adding sql scripts

--- server/api/controllers/User_MessageController.js
+++ server/api/controllers/User_MessageController.js
@@ -1,5 +1,5 @@
 /**
- * UserMessageController
+ * User_MessageController
  *
  * @description :: Server-side logic for managing Usermessages
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
