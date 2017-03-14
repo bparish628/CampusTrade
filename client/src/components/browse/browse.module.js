3096edf11d5709b96219498698ae1a566c286ca7
Benjamin Parish <benjaminparish628@gmail.com>
2017-03-14 13:09:17 -0500

    Fixed up browse screen and connected it to the api

--- client/src/components/browse/browse.module.js
+++ client/src/components/browse/browse.module.js
@@ -1,16 +1,32 @@
 import browseComponent from './browse.component';
+import browseService from './browse.service';
+
+import './browse.scss';
 
 const browse = angular
   .module('components.browse', [])
   .component('browse', browseComponent)
+  .service('BrowseService', browseService)
   .config($stateProvider => {
     'ngInject';
 
     $stateProvider
       .state('browse', {
         url: '/browse',
-        template: '<browse></browse>',
+        template: '<browse posts="$resolve.posts" bind="abx"></browse>',
+        resolve: {
+          posts(BrowseService){
+            'ngInject';
+            return BrowseService.getPosts();
+          }
+        }
       });
+  })
+  .run($api => {
+    $api.add({
+      resource: 'posts',
+      url: '/post'
+    });
   })
   .name;
 
