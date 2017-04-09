b434a21f3e54233b91a8b8de1e2587db3f2d5e1e
Benjamin Parish <benjaminparish628@gmail.com>
2017-04-09 18:09:02 -0500

    Added recently viewed to dashboard

--- client/src/components/dashboard/dashboard.component.js
+++ client/src/components/dashboard/dashboard.component.js
@@ -3,7 +3,36 @@ import template from './dashboard';
 const dashboardComponent = {
   template,
   controller: class DashboardComponent {
+    recentlyViewed = [
+      { name: 'Name1' },
+      { name: 'Name2' },
+      { name: 'Name3' },
+      { name: 'Name4' },
+      { name: 'Name5' },
+      { name: 'Name6' }
+    ];
 
+    inView = []
+
+    constructor() {
+      const numPages = Math.ceil(this.recentlyViewed.length / 4);
+      this.pages = {
+        num: [...Array(numPages).keys()],
+        currentPage: 0,
+        displayedPages: []
+      };
+      this.changeView();
+    }
+
+    changeView(num = 0){
+      if (num === -1 || num === this.pages.num.length) return
+
+      const end = this.recentlyViewed.length < num*4 + 4 ? this.recentlyViewed.length : num*4 + 4
+      const currentlyViewed = this.recentlyViewed.slice(num*4, end);
+
+      this.pages.displayedPages = currentlyViewed;
+      this.pages.currentPage = num;
+    }
   },
 };
 
